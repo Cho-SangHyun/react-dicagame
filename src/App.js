ff8d77ecb6cd2e6054d31fe45753db59db58280d
Cho SangHyun <98shcho@naver.com>
2022-01-23 00:47:08 +0900

    던지기, 처음부터 버튼 추가 및 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import Button from "./Button";
 import Dice from "./Dice";
 
+function random(n){
+    return Math.ceil(Math.random() * n);
+}
+
 function App(){
+    // num : 주사위 숫자
+    // sum : 총점 - 주사위 던질 때마다 나오는 숫자들을 누적
+    // gameHistory - 나왔던 주사위 숫자들을 배열 형태로 기록
+    const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
+    // 던지기 버튼을 누를 때 실행될 함수
+    const handleRollClick = () => {
+        const nextNum = random(6);
+        setNum(nextNum);
+        setSum(sum + nextNum);
+        setGameHistory([...gameHistory, nextNum]);
+    };
+    // 처음부터 버튼을 누를 때 실행될 함수
+    const handleClearClick = () => {
+        setNum(1);
+        setSum(0);
+        setGameHistory([]);
+    };
+
     return (
         <div>
-            <Dice />
+            <div>
+                <Button onClick={handleRollClick}>던지기</Button>
+                <Button onClick={handleClearClick}>처음부터</Button>
+            </div>
+            <div>
+                <h2>나</h2>
+                <Dice color="blue" num={num}/>
+                <h2>총점</h2>
+                <p>{sum}</p>
+                <h2>기록</h2>
+                <p>{gameHistory.join(', ')}</p>
+            </div>
         </div>
     );
 }
--- src/Dice.js
+++ src/Dice.js
@@ -1,9 +1,33 @@
 // import문으로 이미지 파일 이름을 정해서 불러올 수 있다
 // 이렇게 하는 이유? src속성에 직접 때려박으면 엑박으로 나옴
-import diceBlue1 from './assets/dice-blue-1.svg';
+import diceBlue01 from './assets/dice-blue-1.svg';
+import diceBlue02 from './assets/dice-blue-2.svg';
+import diceBlue03 from './assets/dice-blue-3.svg';
+import diceBlue04 from './assets/dice-blue-4.svg';
+import diceBlue05 from './assets/dice-blue-5.svg';
+import diceBlue06 from './assets/dice-blue-6.svg';
+import diceRed01 from './assets/dice-red-1.svg';
+import diceRed02 from './assets/dice-red-2.svg';
+import diceRed03 from './assets/dice-red-3.svg';
+import diceRed04 from './assets/dice-red-4.svg';
+import diceRed05 from './assets/dice-red-5.svg';
+import diceRed06 from './assets/dice-red-6.svg';
+
+const DICE_IMAGES = {
+  blue: [diceBlue01, diceBlue02, diceBlue03, diceBlue04, diceBlue05, diceBlue06],
+  red: [diceRed01, diceRed02, diceRed03, diceRed04, diceRed05, diceRed06],
+};
+
 // Dice 컴포넌트
-function Dice(){
-    return <img src={diceBlue1} alt="주사위" />;
+function Dice(props){
+    const src = DICE_IMAGES[props.color][(props.num) - 1];
+    const alt = `${props.color} ${props.num}`;
+    return <img src={src} alt={alt} />;
+}
+
+Dice.defaultProps = {
+    color: 'blue',
+    num: 1
 }
 
 export default Dice;
