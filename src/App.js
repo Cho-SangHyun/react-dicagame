30f011af4d48d26330d5cd8dd1b99b64d1b5623c
Cho SangHyun <98shcho@naver.com>
2022-01-25 21:55:37 +0900

    code refactoring

--- src/App.js
+++ src/App.js
@@ -7,38 +7,22 @@ function random(n){
 }
 
 function App(){
-    // num : 주사위 숫자
-    // sum : 총점 - 주사위 던질 때마다 나오는 숫자들을 누적
-    // gameHistory - 나왔던 주사위 숫자들을 배열 형태로 기록
-    const [num, setNum] = useState(1);
-    const [sum, setSum] = useState(0);
-    const [gameHistory, setGameHistory] = useState([]);
-    
-    const [otherNum, setOtherNum] = useState(1);
-    const [otherSum, setOtherSum] = useState(0);
-    const [otherGameHistory, setOtherGameHistory] = useState([]);
+    // myHistory - 나왔던 주사위 숫자들을 배열 형태로 기록
+    const [myHistory, setMyHistory] = useState([]);
+    const [otherHistory, setOtherHistory] = useState([]);
 
     // 던지기 버튼을 누를 때 실행될 함수
     const handleRollClick = () => {
-        const nextNum = random(6);
-        setNum(nextNum);
-        setSum(sum + nextNum);
-        setGameHistory([...gameHistory, nextNum]);
+        const nextMyNum = random(6);
+        setMyHistory([...myHistory, nextMyNum]);
 
         const nextOtherNum = random(6);
-        setOtherNum(nextOtherNum);
-        setOtherSum(otherSum + nextOtherNum);
-        setOtherGameHistory([...otherGameHistory, nextOtherNum]);
+        setOtherHistory([...otherHistory, nextOtherNum]);
     };
     // 처음부터 버튼을 누를 때 실행될 함수
     const handleClearClick = () => {
-        setNum(1);
-        setSum(0);
-        setGameHistory([]);
-
-        setOtherNum(1);
-        setOtherSum(0);
-        setOtherGameHistory([]);
+        setMyHistory([]);
+        setOtherHistory([]);
     };
    
     return (
@@ -48,8 +32,8 @@ function App(){
                 <Button onClick={handleClearClick}>처음부터</Button>
             </div>
             <div>
-                <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
-                <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory}/>
+                <Board name="나" color="blue" gameHistory={myHistory}/>
+                <Board name="상대" color="red" gameHistory={otherHistory}/>
             </div>
         </div>
     );
