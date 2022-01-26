7781e77f7ce524da2e4ecacf6be18e0bb590ac1e
Cho SangHyun <98shcho@naver.com>
2022-01-26 22:44:41 +0900

    버튼 css적용

--- src/Dice.js
+++ src/Dice.js
@@ -1,5 +1,7 @@
 // import문으로 이미지 파일 이름을 정해서 불러올 수 있다
 // 이렇게 하는 이유? src속성에 직접 때려박으면 엑박으로 나옴
+// 따라서 이미지 경로들을 처리해야 할 땐 이렇게 import로 처리하고 해주자
+// 예를 들어 인라인 스타일을 입힐 때 url(`${variable}`)이런식으로 처리할 것!
 import diceBlue01 from './assets/dice-blue-1.svg';
 import diceBlue02 from './assets/dice-blue-2.svg';
 import diceBlue03 from './assets/dice-blue-3.svg';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import './index.css'; // index.css에 적힌 내용이 html파일에 style태그가 생성되고 그 내용으로 들어간다.
 
 ReactDOM.render(
     <App />,
